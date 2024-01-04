455274e854a47b81dc31ae3385375f28fa9a3754
shanhe2023 <132747119+shanhe2023@users.noreply.github.com>
2024-01-04 13:58:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,61 +1,48 @@
-import os,re
-from datetime import datetime,timedelta
+import os
+import re
+from datetime import datetime, timedelta
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from litellm import completion
 from feedparser import parse
-import smtplib
-from email.mime.text import MIMEText
-from email.header import Header
 from markdown import markdown
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 
-def sendEmail(message:str,receiver:str=os.environ['MAILTO'],subject:str=''):
-    '''
-    发送邮件的方法
-    :param message:
-    :param receiver:
-    :param subject:
-    :return:
-    '''
-    if len(message)==0:
-        return
-    message=message.replace('<td','<td style="border:1px solid grey;"').replace('<table','<table style="border-collapse:collapse;"')
-    subject=datetime.now().strftime('%Y年%m月%d日')+subject
-    sender = os.environ['MAIL'] #发送的邮箱
-    receiver = receiver.split(';')  #要接受的邮箱（注:测试中发送其他邮箱会提示错误）
-    smtpserver = os.environ['SMTP']
-    username = os.environ['MAIL'] #你的邮箱账号
-    password = os.environ['MAILPWD'] #你的邮箱密码
-    msg = MIMEText(message,'html','utf-8') #中文需参数‘utf-8'，单字节字符不需要
-    msg['Subject'] = Header(subject, 'utf-8') #邮件主题
-    msg['from'] = sender    #自己的邮件地址
-    smtp = smtplib.SMTP()
-    try :
-        smtp.connect(smtpserver) # 链接
-        smtp.login(username, password) # 登陆
-        smtp.sendmail(sender, receiver, msg.as_string()) #发送
-        print('邮件发送成功')
-    except smtplib.SMTPException:
-        print('邮件发送失败')
-    smtp.quit() # 结束
+def send_to_feishu(content):
+    """
+    将内容发送到飞书机器人。
+    :param content: 要发送的内容
+    """
+    webhook_url = "https://open.feishu.cn/open-apis/bot/v2/hook/89dc711e-b581-491c-b3a0-8c4c3cbb8e72"
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "msg_type": "text",
+        "content": {
+            "text": content
+        }
+    }
+    response = requests.post(webhook_url, headers=headers, json=data)
+    if response.status_code == 200:
+        print("消息发送成功")
+    else:
+        print(f"消息发送失败: 状态码 {response.status_code}, 响应 {response.text}")
 
-def sumTweets(prompt:str,lang = '中文',length:int = 10000, model='openai/gpt-3.5-turbo-1106',mail=True,render=True):
+def sumTweets(prompt: str, lang='中文', length: int = 10000, model='openai/gpt-3.5-turbo-1106', render=True):
     '''
-    抓取目标推特AI总结并发邮件
+    抓取目标推特AI总结并发飞书消息
     :param lang:
     :param length:
     :param model:
-    :param mail:
     :param render:
     :return:
     '''
-    users=os.environ['TARGET']
+    users = os.environ['TARGET']
     info: str = os.environ['INFO']
-    nitter:str = os.environ['NITTER']
-    minutes:int = int(float(os.environ['MINS']))
+    nitter: str = os.environ['NITTER']
+    minutes: int = int(float(os.environ['MINS']))
     result = ''
     for user in users.split(';'):
         rss_url = f'https://{nitter}/{user}/rss'
@@ -70,7 +57,7 @@ def sumTweets(prompt:str,lang = '中文',length:int = 10000, model='openai/gpt-3
         if len(df) == 0:
             continue
         for k, v in df.iterrows():
-            pattern = r'<a\s+.*?href="([^"]*https://%s/[^/]+/status/[^"]*)"[^>]*>'%nitter.replace(".",r'\.')
+            pattern = r'<a\s+.*?href="([^"]*https://%s/[^/]+/status/[^"]*)"[^>]*>' % nitter.replace(".", r'\.')
             matches = re.findall(pattern, v['summary'])
             if len(matches) > 0:
                 if matches[0] in df['id'].values:
@@ -91,20 +78,19 @@ def sumTweets(prompt:str,lang = '中文',length:int = 10000, model='openai/gpt-3
                     df.at[k, 'summary'] = re.sub(pattern, "<blockquote>%s</blockquote>" % quote, v['summary'])
         df['content'] = df['published'].str[len('Sun, '):-len(' GMT')] + '[' + df['author'] + ']' + '(' + df[
             'id'].str.replace(nitter, 'x.com') + '): ' + df['summary']
-        # df.to_csv('test.csv', index=False)
         tweets = df['content'].to_csv().replace(nitter, 'x.com')[:length]
-        if len(prompt)<10:
-            prompt =  "<tweets>{tweets}</tweets>\n以上是一些推，你是一名{lang}专栏『{info}最新资讯』的资深作者，请在以上推中，挑选出和『{info}』相关信息(若有)的推,汇编成一篇用markdown排版的{lang}文章，包含发推时间、作者(若有)、推特链接(若有)和推特内容以及你的解读和评论，如果没有{info}相关资讯请回复『NOT FOUND』"
-        prompt =  prompt.format(tweets=tweets,lang=lang,info=info)
+        if len(prompt) < 10:
+            prompt = "<tweets>{tweets}</tweets>\n以上是一些推，你是一名{lang}专栏『{info}最新资讯』的资深作者，请在以上推中，挑选出和『{info}』相关信息(若有)的推,汇编成一篇用markdown排版的{lang}文章，包含发推时间、作者(若有)、推特链接(若有)和推特内容以及你的解读和评论，如果没有{info}相关资讯请回复『NOT FOUND』"
+            prompt = prompt.format(tweets=tweets, lang=lang, info=info)
         print('tweets:', prompt)
         if not 'NOT FOUND' in result:
-            result = result + '\n##%s\n\n'%user + completion(model=model, messages=[{"role": "user", "content": prompt, }], api_key=os.environ['OPENAI_API_KEY'],
+            result = result + '\n##%s\n\n' % user + completion(model=model, messages=[{"role": "user", "content": prompt, }], api_key=os.environ['OPENAI_API_KEY'],
                        base_url=os.environ['API_BASE_URL'])["choices"][0]["message"]["content"]
-    if mail and len(result) > 0:
+    if len(result) > 0:
         if render:
-            result=markdown(result.replace('```','').replace('markdown',''),extensions=['markdown.extensions.tables'])
-        sendEmail(result)
+            result = markdown(result.replace('```', '').replace('markdown', ''), extensions=['markdown.extensions.tables'])
+        send_to_feishu(result)
     return result
 
-if __name__=='__main__':
-    sumTweets(os.environ.get('PROMPT',''),mail=True,render=True)
+if __name__ == '__main__':
+    sumTweets(os.environ.get('PROMPT', ''), render=True)
